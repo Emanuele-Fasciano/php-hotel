3da20c1984633221edb14c8162e5d3f053a3a184
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-03-13 17:10:52 +0100

    fix exercise

--- index.php
+++ index.php
@@ -46,38 +46,44 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!-- BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <table class="table">
-          <thead>
-            <tr>
-                <th scope="col">#</th>
-                <?php foreach($keys_array as $key ) : ?>
-                  <th scope="col"> <?= $key ?> </th>
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <?php foreach($keys_array as $key ) : ?>
+                    <th scope="col"> <?= $key ?> </th>
+                    <?php endforeach ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($hotels as $key => $hotel) : ?>
+                <tr>
+                    <th scope="row"><?= $key + 1 ?></th>
+                    <td><?= $hotel["name"] ?></td>
+                    <td><?= $hotel["description"] ?></td>
+                    <td><?= $hotel["parking"] ? "si" : "no" ?></td>
+                    <td><?= $hotel["vote"] ?></td>
+                    <td><?= $hotel["distance_to_center"] ?></td>
+                </tr>
                 <?php endforeach ?>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach($hotels as $key => $hotel) : ?>
-            <tr>
-              <th scope="row"><?= $key + 1 ?></th>
-            <?php foreach($hotel as $detail) : ?>
-              <td><?= $detail ?></td>
-            <?php endforeach ?>
-            </tr>
-            <?php endforeach ?>
-          </tbody>
-       </table>
-   </div>
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
